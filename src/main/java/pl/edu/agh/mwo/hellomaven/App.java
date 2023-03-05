832a3e0d1187cf6a371db73195663dc6d21aff4c
Marta Mlynska <martiimly@gmail.com>
2023-03-05 11:17:34 +0100

    exe 4 ready

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -6,15 +6,19 @@ import com.indvd00m.ascii.render.api.IContextBuilder;
 import com.indvd00m.ascii.render.api.IRender;
 import com.indvd00m.ascii.render.elements.PseudoText;
 
-/**
+/*
  * Simple main application
  */
 public class App {
 
+    public static final int TEXT_WIDTH = 120;
+    public static final int TEXT_HEIGHT = 20;
+    public static final int RANDOM_NUMBER = 4;
+
     public static void main(String[] args) {
         IRender render = new Render();
         IContextBuilder builder = render.newBuilder();
-        builder.width(120).height(20);
+        builder.width(TEXT_WIDTH).height(TEXT_HEIGHT);
         builder.element(new PseudoText("Hello Maven!"));
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
@@ -24,6 +28,6 @@ public class App {
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return RANDOM_NUMBER;
     }
 }
